a75fb1d349b754d3f7858c07e199904f38f322b3
Luojiaji <lt920@126.com>
2019-12-03 21:43:51 +0800

    add comments in data_split

--- data_split.py
+++ data_split.py
@@ -5,14 +5,16 @@ dataset_path = './data_set/'
 
 
 def get_datalist(datapath):
-    
+    # get category 
     filename = os.listdir(datapath)
     datafile = []
     label = []
+    # number of all data
     cnt = 0
     print('number of category:', len(filename), filename)
     print('-'*40)
 
+    # get data file path of each category
     for i,path in enumerate(filename):
         dataname  = os.listdir(os.path.join(datapath, path))
         print(path,':',len(dataname))
@@ -34,9 +36,12 @@ def get_datalist(datapath):
 
 def data_split_by_number(datafile, label):
     l = len(datafile)
-    split_num = [100, 100, 100, 100, 100]
+    # number of test data of each category
+    split_num = [100, 100, 100, 100, 100] 
     test_data_path = []
     test_label = []
+    
+    # random sampling in each category
     for i in range(l):
         num = len(datafile[i])
         curr_test_data_path = []
@@ -45,18 +50,22 @@ def data_split_by_number(datafile, label):
         while len(ind_set) != split_num[i]:
             ind_set.add(random.randrange(num))
 
+        # random sampling
         for ind in ind_set:
             curr_test_data_path.append(datafile[i][ind])
             curr_test_label.append(i)
         
+        # delet the test data in train dataset
         for p in curr_test_data_path:
             datafile[i].remove(p)
             label[i].remove(i)
         test_data_path.append(curr_test_data_path) 
         test_label.append(curr_test_label)
+    
     train_data_path = datafile
     train_label = label
     
+    # print the test number and train number of each category
     for i in range(len(datapath)):
         print('test number:', len(test_data_path[i]), 
             'train number:', len(train_data_path[i]))
